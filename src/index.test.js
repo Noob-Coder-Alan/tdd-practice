df2dd6d303c89716277107b77527773d7e799135
Alanray Baltazar Elangos <48097631+Noob-Coder-Alan@users.noreply.github.com>
2020-11-16 18:42:10 +0800

    refactor: minor changes to test code

--- src/index.test.js
+++ src/index.test.js
@@ -5,13 +5,12 @@ describe('getBMI', () => {
     it('It gets the BMI using units in metric', () => {
         expect(getBMI(170, 60, "metric")).toBe(20.8)
     });
-});
 
-describe('getBMI', () => {
     it('It gets the BMI using units in english', () => {
         expect(getBMI("5'7", 130,"english")).toBe(20.4)
     });
 });
+
 
 describe('exchangeClockwise', () => {
     it('(Odd no of students) Gets the recommended order of exchanging papers in clockwise.', () => {
