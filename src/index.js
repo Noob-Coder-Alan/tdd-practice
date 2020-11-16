e9f2e574efdf5cff96b95005b69c52684cef24f9
Alanray Baltazar Elangos <48097631+Noob-Coder-Alan@users.noreply.github.com>
2020-11-16 16:25:36 +0800

    feat: added code that workds to getBMI function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-function getBMI(h, m, system){
-
+function getBMI(h, w, system){
+    if(system === "english"){
+        const ftInch = h.split("'")
+        const height = Number(ftInch[0])*12 + Number(ftInch[1])
+        return Math.round((703*(w/height**2))*10)/10
+    }
+    else if(system === "metric"){
+        return Math.round((w/(h/100)**2)*10)/10
+    }
 }
-
 export default getBMI;
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,13 @@
 import getBMI from './index'
+
 describe('getBMI', () => {
-    it('It gets the BMI', () => {
-        expect(getBMI(1,1)).toBe(2)
+    it('It gets the BMI using units in metric', () => {
+        expect(getBMI(170, 60, "metric")).toBe(20.8)
+    });
+});
+
+describe('getBMI', () => {
+    it('It gets the BMI using units in english', () => {
+        expect(getBMI("5'7", 130,"english")).toBe(20.4)
     });
 });
